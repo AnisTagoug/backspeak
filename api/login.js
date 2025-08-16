f674378b28c530d5761adf512aefe43d55873ec3
DonSopra <anistagoug@gmail.com>
2025-08-16 18:06:14 +0100

    last

--- api/login.js
+++ api/login.js
@@ -25,10 +25,20 @@ async function query(text, params) {
 }
 
 module.exports = async function handler(req, res) {
-  // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Enable CORS for specific origins
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://speakupsite.vercel.app'
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   
   if (req.method === 'OPTIONS') {
     res.status(200).end();
